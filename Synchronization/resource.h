f2ef2868a72d27c2eda223257ce238b1eb0f278f
Denis Maltsev <deni@rionix.com>
2017-05-17 18:23:19 +0400

    synchronization

---
+++ Synchronization/resource.h
@@ -0,0 +1,5 @@
+#define IDM_MAINMENU                    1000
+#define IDM_ABOUT						1001
+#define IDM_EXIT                        1002
+#define IDM_RUNTHREADS					1003
+#define IDC_STATIC                      -1
