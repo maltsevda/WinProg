a46ec6236e4639bb37de25f9d822d3e366312240
Denis Maltsev <deni@rionix.com>
2018-05-27 13:13:48 +0400

    example of GDI error

---
+++ ErrorGDI/.vscode/compile.bat
@@ -0,0 +1,13 @@
+@echo off
+
+if exist "%PROGRAMFILES(X86)%" (
+	call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
+) else (
+	call "C:\Program Files\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
+)
+
+cd /D "%1"
+nmake /NOLOGO || goto end
+
+:end
+rem Nothing here
